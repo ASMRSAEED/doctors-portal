7a7dddab008bffe19e1ef5851a588dbd086073c6
ASMRSAEED <asmrsaeed@gmail.com>
2022-05-14 02:15:02 +0600

    appointment created

---
+++ src/Pages/Home/MakeAppointment.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import doctor from '../../assets/images/doctor.png'
+import appointment from '../../assets/images/appointment.png'
+import PrimaryButton from '../Shared/PrimaryButton';
+
+const MakeAppointment = () => {
+    return (
+        <section style={{
+            background: `url(${appointment})`
+        }}
+            className='flex justify-center items-center'>
+            <div className='flex-1 hidden lg:block'>
+                <img className='mt-[-150px]' src={doctor} alt="" />
+            </div>
+            <div className='flex-1'>
+                <h3 className='text-xl text-primary font-bold'>Appointment</h3>
+                <h2 className='text-3xl text-white'>Make an Appointment Today</h2>
+                <p className='text-white'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi fugiat sunt repudiandae ex iste atque, consequuntur ducimus quas ut, minus obcaecati hic molestias? Nemo nam sequi voluptates distinctio debitis officiis in soluta, animi adipisci delectus suscipit et reiciendis doloribus. Pariatur?</p>
+                <PrimaryButton>Get Started</PrimaryButton>
+            </div>
+        </section>
+    );
+};
+
+export default MakeAppointment;
