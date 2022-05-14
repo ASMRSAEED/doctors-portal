4db22c68a9a1de30ccc9e14bd9091c548b076de7
ASMRSAEED <asmrsaeed@gmail.com>
2022-05-14 23:52:26 +0600

    appointment created

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import chair from '../../assets/images/chair.png'
 import PrimaryButton from '../Shared/PrimaryButton';
+import bg from '../../assets/images/bg.png'
 
 const Banner = () => {
     return (
-        <div className="hero min-h-screen">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src={chair} className="max-w-sm rounded-lg shadow-2xl" />
-                <div>
-                    <h1 className="text-5xl font-bold">Your New Smile Start Here</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <PrimaryButton>Get Started</PrimaryButton>
+        <div
+            className='hero min-h-screen'
+            style={{ backgroundImage: `url(${bg})` }}>
+            <div className='hero-content flex-col flex flex-row justify-between lg:flex-row-reverse'>
+                <img
+                    src={chair}
+                    alt='doctors portal'
+                    className='max-w-sm rounded-lg shadow-2xl basis-2/4'
+                />
+                <div className='basis-2/4'>
+                    <h1 className='text-5xl font-bold'>Your New Smile Starts Here!</h1>
+                    <p className='py-6'>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting
+                        industry. Lorem Ipsum has been the industry's standard dummy text
+                        ever since the
+                    </p>
+                    <PrimaryButton>GET STARTED</PrimaryButton>
                 </div>
             </div>
         </div>
--- src/Pages/Home/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment.js
@@ -12,10 +12,10 @@ const MakeAppointment = () => {
             <div className='flex-1 hidden lg:block'>
                 <img className='mt-[-150px]' src={doctor} alt="" />
             </div>
-            <div className='flex-1'>
+            <div className='flex-1 px-5'>
                 <h3 className='text-xl text-primary font-bold'>Appointment</h3>
-                <h2 className='text-3xl text-white'>Make an Appointment Today</h2>
-                <p className='text-white'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi fugiat sunt repudiandae ex iste atque, consequuntur ducimus quas ut, minus obcaecati hic molestias? Nemo nam sequi voluptates distinctio debitis officiis in soluta, animi adipisci delectus suscipit et reiciendis doloribus. Pariatur?</p>
+                <h2 className='text-3xl text-white py-5'>Make an Appointment Today</h2>
+                <p className='text-white pb-5'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi fugiat sunt repudiandae ex iste atque, consequuntur ducimus quas ut, minus obcaecati hic molestias? Nemo nam sequi voluptates distinctio debitis officiis in soluta, animi adipisci delectus suscipit et reiciendis doloribus. Pariatur?</p>
                 <PrimaryButton>Get Started</PrimaryButton>
             </div>
         </section>
