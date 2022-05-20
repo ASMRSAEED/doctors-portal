edeb0a8fc0e43f3f3fbc0aebe187abab62b1d174
ASMRSAEED <asmrsaeed@gmail.com>
2022-05-21 04:02:57 +0600

    admin panel created

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -48,7 +48,7 @@ const BookingModal = ({ date, treatment, setTreatment, refetch }) => {
             <input type="checkbox" id="booking-modal" className="modal-toggle" />
             <div className="modal modal-bottom sm:modal-middle">
                 <div className="modal-box">
-                    <label for="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                    <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 className="font-bold text-lg text-secondary text-center">Booking For: {name}</h3>
                     <form onSubmit={handleBooking} className='grid grid-cols-1 gap-4 justify-items-center mt-5'>
                         <input type="text" disabled value={format(date, 'PP')} className="input input-bordered w-full max-w-xs" />
--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -4,6 +4,7 @@ import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
 import Loading from '../Shared/Loading';
 import { Link, useNavigate } from 'react-router-dom';
+import useToken from '../../hooks/useToken';
 
 const SignUp = () => {
     const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
@@ -17,6 +18,8 @@ const SignUp = () => {
 
     const [updateProfile, updating, updateError] = useUpdatePassword(auth);
 
+    const [token] = useToken(gUser || user);
+
     const navigate = useNavigate();
 
     let signInError;
@@ -29,16 +32,14 @@ const SignUp = () => {
         signInError = <p className='text-red-500'><small>{gError?.message || error?.message || updateError?.message}</small></p>
     }
 
-    if (gUser || user) {
-        console.log(gUser || user);
+    if (token) {
+        navigate('/appointment');
     }
 
     const onSubmit = async data => {
-        console.log(data);
         await CreateUserWithEmailAndPassword(data.email, data.password);
         await updateProfile({ displayName: data.name });
-        console.log('Update Done');
-        navigate('/appointment')
+
 
     }
 
